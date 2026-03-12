31f9c733cf2119c8cf824f50c623a4f990f4580d
Y0sefTamer <programmeryosef@gmail.com>
2026-03-12 10:52:14 +0200

    create the project

---
+++ src/Ethereum/MinimalAccount.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/// @title MinimalAccount
+/// @notice A minimal account contract that can receive and send Ether.
+contract MinimalAccount {}

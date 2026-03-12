5d2e86034ec9488f05107fb6166c128c69ca06b6
Y0sefTamer <programmeryosef@gmail.com>
2026-03-12 11:05:49 +0200

    adding import IAccount contract and make our contract inhert from it

--- src/Ethereum/MinimalAccount.sol
+++ src/Ethereum/MinimalAccount.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+
 /// @title MinimalAccount
 /// @notice A minimal account contract that can receive and send Ether.
-contract MinimalAccount {}
+contract MinimalAccount is IAccount {}

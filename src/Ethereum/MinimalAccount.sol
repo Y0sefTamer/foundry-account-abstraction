5097daed5b7b8f37a69084d44c08b89e6d47179d
Y0sefTamer <programmeryosef@gmail.com>
2026-03-12 11:09:20 +0200

    add essential finction and import PackedUserOperation

--- src/Ethereum/MinimalAccount.sol
+++ src/Ethereum/MinimalAccount.sol
@@ -2,7 +2,16 @@
 pragma solidity 0.8.20;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
 /// @title MinimalAccount
 /// @notice A minimal account contract that can receive and send Ether.
-contract MinimalAccount is IAccount {}
+contract MinimalAccount is IAccount {
+
+
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData){}
+}
